ba279a2d33eea73407e7f6f9a10a0b55cf478f5a
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-07-15 18:46:08 +0530

    added

---
+++ Test.java
@@ -0,0 +1,12 @@
+class Test {
+
+    public static void main(String[] args) {
+        if (true) {
+            System.out.println("hey");
+        } else {
+            System.out.println("hello");
+        }
+
+    }
+
+}
