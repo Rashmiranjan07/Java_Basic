282e66713fe19b1060237ee1fafe4fd98a52ad2a
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-07-15 19:19:36 +0530

    added

--- Test.java
+++ Test.java
@@ -1,3 +1,5 @@
+// Here the excution of the blocks will be done on the basis of the condition (true/false) present in the begning of the "if-block", where if block will be excuted if the output of the condition is "true" & else block will be excuted if the output of the consition is false .
+
 class Test {
 
     public static void main(String[] args) {
@@ -6,6 +8,104 @@ class Test {
         } else {
             System.out.println("hello");
         }
+
+    }
+
+}
+
+/* In the condition part either we can write boolean expression (true/false) directly or we can perfrom an operation whose output will be in the form of boolean expresion  */
+
+class Test {
+
+    public static void main(String[] args) {
+        if (10 > (10 + 2)) {
+            System.out.println("hey");
+        } else {
+            System.out.println("hello");
+        }
+
+    }
+
+}
+
+/*Else is optional  */
+class Test
+{
+
+	public static void main (String[]args)
+	{
+		if(true)
+		{
+			System.out.println("hey");
+		}
+
+	}
+
+}
+
+// ex-2 
+
+class Test
+{
+
+	public static void main (String[]args)
+	{
+		if(false)
+		{
+			System.out.println("if block");
+		}
+		    System.out.println("Main method");
+
+	}
+
+}
+
+/*We can not write any statement in between if & else , if we are doing so , then we will get compilation error .*/
+
+class Test
+{
+
+	public static void main (String[]args)
+	{
+		if(true)
+		{
+			System.out.println("if block");
+		}
+
+		    System.out.println("Main method");   //compilation error 
+		else {
+			System.out.println("bce");
+
+		}
+		System.out.println("bce");
+	}
+
+}
+
+/*curly braces {} are optional in if-else statement  */
+
+class Test {
+
+    public static void main(String[] args) {
+        if (true)
+
+            System.out.println("if block"); 
+
+        System.out.println("i am a boy");
+
+    }
+
+}
+
+/* If we are not giving any curly braces {} then the very next statement as the part of the if block & that is the only statement which will be considered */
+
+class Test {
+
+    public static void main(String[] args) {
+        if (false)
+
+            System.out.println("if block");
+            System.out.println("i am a boy"); // i am a boy 
 
     }
 
