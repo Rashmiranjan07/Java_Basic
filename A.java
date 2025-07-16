d602ed917979909abcc12928d63b83aa1e2ac713
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-07-16 21:22:08 +0530

    added

---
+++ A.java
@@ -0,0 +1,34 @@
+
+// WAP to check whether in number is even or odd ?
+public class A {
+    public static void main (String[]args)
+	{
+		int num=10;
+		if (num%2==0) 
+		{
+			System.out.println("even");
+		}
+		else {
+			System.out.println("odd");
+		}
+			
+	}
+}
+
+// WAP to check whether a person is eligible to vote or not ?
+ class Vote {
+	public static void main(String[]args)
+	{
+		int age = 20;
+		if(age>=18)
+		{
+			System.out.println("Eligible");
+		}
+		else {
+			System.out.println("not eligble");
+		}
+	}
+ }
+
+
+
