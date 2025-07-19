afac0039334b958c4293f86228016c762168bc95
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-07-19 22:49:33 +0530

    added

--- pattern.java
+++ pattern.java
@@ -1,28 +1,30 @@
 
+// print the patttern by user input 
+// *******
+// *******
+// *******
+// *******
+// *******
+// *******
+// *******
+import java .util.Scanner;
 class pattern
 {
-	public static void main(String[]args){
-		int rows = 5;
-		for(int i=1; i<=rows;i++) {
-			for(int j=1; j<=rows; j++) {
-				System.out.print("*");
-			}
-			 System.out.println();
+	public static void main(String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of n");
+		int n= sc.nextInt();
+		for(int i=0; i<n; i++)
+	{  
+		  for(int j=0; j<n; j++)
+		{
+			System.out.print("*");
 		}
-	}
+		System.out.println( );
+	 }
+   }  
 }
 
-class pattern2
-{
-	public static void main(String[]args){
-		int rows = 5;
-		for(int i=1; i<=rows;i++) {
-			for(int j=1; j<=i; j++) {
-				System.out.print("*");
-			}
-			 System.out.println();
-		}
-	}
-}
 
 
