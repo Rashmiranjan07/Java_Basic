17b36f0540bb0720c189de8c217ce0ce894dbe2f
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-07-21 21:00:19 +0530

    achanges

--- Allpatterns.java
+++ Allpatterns.java
@@ -295,3 +295,39 @@ class Pattern10
 	}
 			
 }
+
+//Print the pattern 
+
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+//    **
+//   ****
+//  ******
+// ********
+
+
+class Pattern11
+{
+	public static void main (String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of n");
+		int n= sc.nextInt();
+		for(int i=0; i<n; i++)			
+		{
+			for(int j=0; j<n; j++) 
+			{
+				
+				if((i+j<=n-1 && i<=j)||(i>j && i+j>=n-1))
+					System.out.print("*");
+				else
+				System.out.print(" ");
+			}
+			System.out.println( );
+		}
+	}
+			
+}
