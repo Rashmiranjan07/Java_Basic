1d7f169e5998819de96935635f8db6b10156b15c
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-07-22 22:28:38 +0530

    added

--- Allpatterns.java
+++ Allpatterns.java
@@ -588,3 +588,32 @@ class Pattern18
 	}
 					
 }
+
+// 🗼 Eiffel Tower print 
+class Tower
+{
+	public static void main (String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of n");
+		int n= sc.nextInt();
+		for(int i=0; i<n; i++)			
+		{
+			for(int j=0; j<n; j++) 
+			{
+				
+				if(j==n/2 || 
+					i+j==n-1 && i>=n/2 ||
+					i==j && j>=n/2)
+					
+					System.out.print("*");
+				else
+				System.out.print(" ");
+			}
+			System.out.println( );
+		}
+	}
+					
+}
+
+
