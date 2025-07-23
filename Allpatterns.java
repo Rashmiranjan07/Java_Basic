47ede918283ebcb677829ac1aae4e2d8ad654357
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-07-23 08:24:26 +0530

    added

--- Allpatterns.java
+++ Allpatterns.java
@@ -617,3 +617,68 @@ class Tower
 }
 
 
+
+
+// Print the pattern
+// a      
+// bb
+// ccc
+// dddd
+// eeeee
+// ffffff
+
+class Pattern19
+{
+	public static void main (String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of n");
+		int n= sc.nextInt();
+		for(int i=0; i<n; i++)			
+		{
+			for(int j=0; j<n; j++) 
+			{
+				
+				if (i>=j)
+					System.out.print((char)(i+97));
+				else
+				System.out.print(" ");
+			}
+			System.out.println( );
+		}
+	}
+					
+}
+
+
+
+// Print the pattern
+// a      
+// ab     
+// abc    
+// abcd   
+// abcde  
+
+class Pattern20
+{
+	public static void main (String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of n");
+		int n= sc.nextInt();
+		for(int i=0; i<n; i++)			
+		{
+			for(int j=0; j<n; j++) 
+			{
+				
+				if (i>=j)
+					System.out.print((char)(j+97));
+				else
+				System.out.print(" ");
+			}
+			System.out.println( );
+		}
+	}
+					
+}
+
