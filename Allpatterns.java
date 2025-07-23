d4525f3ceacf44005c0be458741494652b03f13b
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-07-23 08:27:36 +0530

    added

--- Allpatterns.java
+++ Allpatterns.java
@@ -682,3 +682,33 @@ class Pattern20
 					
 }
 
+
+// Print the pattern 
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
+class Pattern21
+{
+	public static void main (String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of n");
+		int n= sc.nextInt();
+		int k=1;
+		for(int i=0; i<n; i++)			
+		{
+			for(int j=0; j<n; j++) 
+			{
+				
+				if (i>=j)
+					System.out.print(k++ + " ");
+				else
+				System.out.print(" ");
+			}
+			System.out.println( );
+		}
+	}
+					
+}
