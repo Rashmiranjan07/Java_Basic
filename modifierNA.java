b95ee267e31dbebbcfc65ee1ca83e4514934913f
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-08-09 21:51:58 +0530

    added

--- modifierNA.java
+++ modifierNA.java
@@ -1,7 +1,37 @@
 // non-acess modifier :- the modifier which deals with the functanality of only class , methods & variables , is known as non acess variables.
 
 
-//final
-class NA1 {
-    
+//final:-
+// 1. if a method declared as final then we can not override that method .
+class A1 {
+    final void m1(){
+        System.out.println("hii");
+    }
+}
+
+// //class B1 extends A1 {
+//     public void m1(){
+//         System.out.println("hello");
+//     }
+// }
+
+
+class Test1 {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+// 2. if a method is declared as final then it is not mandatory to declare the class as final.
+
+class A2 {
+    final void m1(){
+        System.out.println("hii");
+    }
+}
+class Test2 {
+    public static void main(String[] args) {
+        
+    }
 }
