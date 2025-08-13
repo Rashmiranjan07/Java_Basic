2e528f3be2271f3758aea0b8389d7cdfbf558a30
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-08-13 11:06:36 +0530

    added

---
+++ constructor.java
@@ -0,0 +1,12 @@
+// Constyructor 
+// it is a special type of method which is getting excuted at the time of object creation .
+class C1 {
+    C1()
+    {
+        System.out.println("heyy");
+    }
+    public static void main(String[] args) {
+        C1 t1=new C1();  // heyy
+    }
+}
+
