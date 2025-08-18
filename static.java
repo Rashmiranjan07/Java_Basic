2e33d5f736368bc0c7815e2d4101541dfb706fff
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-08-18 22:35:02 +0530

    added

--- static.java
+++ static.java
@@ -2,9 +2,25 @@
 // Static block will excuted at the time of class loading .
 class B1 {
     static {
-        System.out.println("static block-1");
+        System.out.println("static block-1"); // static block
     }
     public static void main(String[] args) {
-        System.out.println("main");
+        System.out.println("main"); // main
+    }
+}
+
+
+//We can write more than one static block inside a class but the flow of excution of the static block will be from top to bottom .
+
+
+class B2 {
+     static {
+        System.out.println("static block-1"); // static block-1
+    }
+    public static void main(String[] args) {
+        System.out.println("main"); //main
+    }
+     static {
+        System.out.println("static block-2"); // static block -2
     }
 }
