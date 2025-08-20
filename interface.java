2ea2a3930e374fe17710ac4361d4b7d9e163919b
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-08-20 23:43:49 +0530

    added

--- interface.java
+++ interface.java
@@ -1,10 +1,24 @@
 // * write a program to prove the interface variable are bydefaultly static ?
+//pg1:-
 interface A {
     int x = 10;
 }
 
 class i1 {
     public static void main(String[] args) {
-        System.out.println(A.x); //10
+        System.out.println(A.x); // 10
+    }
+}
+
+// pg2:-
+interface B {
+    int x = 10;
+}
+
+class Test1 implements B {
+    public static void main(String[] args) {
+        System.out.println(B.x); // 10
+        System.out.println(x); // 10
+        System.out.println(Test1.x); // 10
     }
 }
