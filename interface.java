43eb2aa0b22d5897a94db4b71aa870d507153ebc
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-08-20 23:55:15 +0530

    added

--- interface.java
+++ interface.java
@@ -21,28 +21,24 @@ class Test1 implements B {
         System.out.println(x); // 10
         System.out.println(Test1.x); // 10
 
-        Test1 t1=new Test1();
-        System.out.println(t1.x); //10
+        Test1 t1 = new Test1();
+        System.out.println(t1.x); // 10
     }
 }
 
-
-
-// Write a program to prove the interface variable is bydefaultly final 
+// Write a program to prove the interface variable is bydefaultly final
 
 // interface C {
 
-//     int x=10;
+// int x=10;
 // }
 // class Test2{
-//     public static void main(String[] args) {
-//         C.x=20; // CE
-//     }
+// public static void main(String[] args) {
+// C.x=20; // CE
+// }
 // }
 
-
-
-//pg3:-
+// pg3:-
 interface C {
     int x = 10;
 }
@@ -50,13 +46,54 @@ interface C {
 class Test2 implements C {
 
     int x = 20;
+
     public static void main(String[] args) {
 
-         Test2 t1=new Test2();
-        System.out.println(t1.x); //20
+        Test2 t1 = new Test2();
+        System.out.println(t1.x); // 20
 
         System.out.println(C.x); // 10
-       
-       
+
+    }
+}
+
+// pg3:-
+interface D {
+    int x = 10;
+}
+
+class Test3 implements D {
+
+    int x = 20;
+
+    public static void main(String[] args) {
+        int x = 30;
+        System.out.println(x); // 30
+        Test3 t1 = new Test3();
+        System.out.println(t1.x); // 20
+
+        System.out.println(D.x); // 10
+
+    }
+}
+
+
+// pg4:-
+interface E {
+    int x = 10;
+}
+
+class Test4 implements E {
+
+    public static void main(String[] args) {
+        int x = 30;
+        x = 40;
+        System.out.println(x); // 40
+        System.out.println(Test4.x); // 10
+
+        System.out.println(E.x); // 10
+        Test4 t1 = new Test4();
+        System.out.println(t1.x); // 20
+
     }
 }
