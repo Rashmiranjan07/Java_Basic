1dce41cfda3ed2d1d718096e6327166fd0a79c55
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-09-22 12:22:04 +0530

    added

---
+++ Arrays.java
@@ -0,0 +1,16 @@
+ // Anynymous object
+ 
+ class Arrays {
+    public static void main(String[] args) {
+        int [] arr ={10,20,30};
+        sum(arr);
+        sum(new int[]{10,20,30});
+    }
+    public static void sum(int[]x) {
+        int sum =0;
+        for(int i=0; i<x.length;i++){
+            sum+=x[i];
+        }
+        System.out.println(sum);
+    }
+}
