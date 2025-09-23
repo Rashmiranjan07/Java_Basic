f4cbdfaa6f9787924d99a1997ebe4d34b2960237
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-09-23 10:41:51 +0530

    added

--- Arrays.java
+++ Arrays.java
@@ -37,22 +37,22 @@ class Test {
     }
 }
 
-// WAP to find sum of all element from an array or by using variable -argument method .
+// WAP to find sum of all element from an array or by using variable -argument
+// method .
 class Test01 {
-	public static void main(String[] args) {
-		int a[]= {20,40,60};
-		
-		int res=m1(a);
-		System.out.println(res);
-		//System.out.println(m1(a));
-		
-	}
-	
-	static int m1(int...a) {
-		int sum=0;
-		for(int i=0; i<a.length;i++) {
-			sum+=a[i];
-		}
-		return sum;
-	}
+    public static void main(String[] args) {
+        int a[] = { 20, 40, 60 };
+
+        int res = m1(a);
+        System.out.println(res);
+
+    }
+
+    static int m1(int... a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        return sum;
+    }
 }
