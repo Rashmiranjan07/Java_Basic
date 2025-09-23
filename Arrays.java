a716ca131df193535c2de8d92e9f7172cc660c2c
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-09-23 10:44:29 +0530

    added

--- Arrays.java
+++ Arrays.java
@@ -57,23 +57,22 @@ class Test01 {
     }
 }
 
-
-// we can use array also in the  argument part 
+// we can use array also in the argument part
 class Test02 {
-	public static void main(String[] args) {
-		int a[]= {20,40,60};
-		
-		int res=m1(a);
-		System.out.println(res);
-		//System.out.println(m1(a));
-		
-	}
-	
-	static int m1(int[]a) {
-		int sum=0;
-		for(int i=0; i<a.length;i++) {
-			sum+=a[i];
-		}
-		return sum;
-	}
+    public static void main(String[] args) {
+        int a[] = { 20, 40, 60 };
+
+        int res = m1(a);
+        System.out.println(res);
+        
+
+    }
+
+    static int m1(int[] a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        return sum;
+    }
 }
