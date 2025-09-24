855b9498d40f98eb87632cb25059d03d07fbf81a
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-09-24 20:50:27 +0530

    newly added

---
+++ Arrayprogram/SumOfCharDigit.java
@@ -0,0 +1,17 @@
+package Arrayprogram;
+
+public class SumOfCharDigit {
+	public static void main(String[] args) {
+		String s="1su23bhka41ti";
+		int sum=0;
+		for(int i=0; i<s.length(); i++) {
+			char ch=s.charAt(i);
+			
+			if(ch>='0' && ch<='9' ) {
+				sum= sum+ (ch-'0'); //sum=sum+(ch-48);
+			}
+		}
+		System.out.println(sum);
+	}
+
+}
