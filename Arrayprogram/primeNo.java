7b5222797798b969f5d2b9cdc15c8407b6441da0
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-09-25 12:15:35 +0530

    newly added

--- Arrayprogram/primeNo.java
+++ Arrayprogram/primeNo.java
@@ -1,24 +1,27 @@
+// Write a java program to print all the prime number of an array?
+
 package Arrayprogram;
 
 public class primeNo {
 	public static void main(String[] args) {
-		int a[]= {1,2,3,4,5,6,7,8,9,10};
-		for(int temp:a) {
-			if(isPrime(temp)) {
-			System.out.println(temp);
+		int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		for (int temp : a) {
+			if (isPrime(temp)) {
+				System.out.println(temp);
+			}
 		}
 	}
-	}
+
 	static boolean isPrime(int n) {
-		if(n<=1)
-			
-			 return false;
-		int count=0;
-		for(int i=1; i<=n; i++) {
-			if(n%i==0)
+		if (n <= 1)
+
+			return false;
+		int count = 0;
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0)
 				count++;
 		}
-		return count==2;
+		return count == 2;
 	}
 
 }
