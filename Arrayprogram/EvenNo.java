b9019c992869e7e09f553d34e4f0a48e353c9658
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-09-25 12:17:15 +0530

    newly added

--- Arrayprogram/EvenNo.java
+++ Arrayprogram/EvenNo.java
@@ -1,15 +1,16 @@
+// Write a java program to print only the even number of an array?
+
 package Arrayprogram;
 
 public class EvenNo {
 	public static void main(String[] args) {
-		
-		int a[]= {1,2,3,4,5,6,7,8,9,10};
-		for(int temp:a) {
-			if(temp%2==0) {
+
+		int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		for (int temp : a) {
+			if (temp % 2 == 0) {
 				System.out.println(temp);
 			}
 		}
-		
 
 	}
 }
