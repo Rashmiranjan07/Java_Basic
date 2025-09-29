6659e3ce73e1130bfeb8020381b9ed6ae9094fc3
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-09-29 23:57:46 +0530

    newly added

--- Arrayprogram/Maxnumber.java
+++ Arrayprogram/Maxnumber.java
@@ -1,15 +1,17 @@
+// Write a java program to find maximum no of element in an array .
+
 package Arrayprogram;
 
 public class Maxnumber {
-	public static void main(String[] args) {
-		int a []= {2,3,9,5,6,10,4};
-		int max =Integer.MIN_VALUE;
-		for(int temp:a) {
-			if(temp>max) {
-				max=temp;
-			}
-		}
-		System.out.println(max);
-	}
+    public static void main(String[] args) {
+        int a[] = { 2, 3, 9, 5, 6, 10, 4 };
+        int max = Integer.MIN_VALUE;
+        for (int temp : a) {
+            if (temp > max) {
+                max = temp;
+            }
+        }
+        System.out.println(max);
+    }
 
 }
