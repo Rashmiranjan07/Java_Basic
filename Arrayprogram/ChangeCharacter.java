90b8e54962836d7e52c8fd1b40a64a02685cc3a6
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-09-29 22:48:36 +0530

    newly added

---
+++ Arrayprogram/ChangeCharacter.java
@@ -0,0 +1,20 @@
+package Arrayprogram;
+
+public class ChangeCharacter {
+	public static void main(String[] args) {
+		String res=changeChar("This is java class",'a','*');
+		System.out.println(res);
+	}
+	
+	public static String changeChar(String s, char oldChar,char newChar) {
+		char ch[]=s.toCharArray();
+		for (int i=0; i<ch.length;i++) {
+			if(ch[i]==oldChar) {
+				ch[i]=newChar;
+			}
+		}
+		String res=new String(ch);
+		return res;
+	}
+
+}
