80e05df0b0d428198912b0fbb80a6da9165abbf4
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-09-29 23:07:50 +0530

    newly added

---
+++ Arrayprogram/ReverseString.java
@@ -0,0 +1,24 @@
+package Arrayprogram;
+
+public class ReverseString {
+	public static void main(String[] args) {
+		String res=reverse("Ronaldo");
+		System.out.println(res);
+	}
+	
+	public static String reverse(String s) {
+		char ch[]=s.toCharArray();
+		int i=0;
+		int j=s.length()-1;
+		while(i<j) {
+			char temp=ch[i];
+			ch[i]=ch[j];
+			ch[j]=temp;
+			i++;
+			j--;
+		}
+		
+		return new String(ch);
+	}
+
+}
