5f6f7b628d9caf7d1d43044b256b0b5ab98f50ee
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-09-29 23:55:21 +0530

    newly added

--- Arrayprogram/DeleteLast.java
+++ Arrayprogram/DeleteLast.java
@@ -1,20 +1,19 @@
+//Write a java program to delete an element at the last index of an aray 
+
 package Arrayprogram;
 
 import java.util.Arrays;
 
 public class DeleteLast {
-	public static void main(String[] args) {
-		int a[]= {2,3,9,5,6,1};
-		int b[]=new int[a.length-1];
-		for (int i=0;i<b.length;i++) {
-			b[i]=a[i];
-		}
-		a=b;
-		System.out.println(Arrays.toString(a));
-		
-	}
+    public static void main(String[] args) {
+        int a[] = { 2, 3, 9, 5, 6, 1 };
+        int b[] = new int[a.length - 1];
+        for (int i = 0; i < b.length; i++) {
+            b[i] = a[i];
+        }
+        a = b;
+        System.out.println(Arrays.toString(a));
+
+    }
 
 }
-
-
-
