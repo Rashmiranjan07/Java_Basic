d395526c802af225c38d26e0cb6da4d6ebd4b084
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-09-29 23:50:31 +0530

    newly added

---
+++ Arrayprogram/DeleteFirst.java
@@ -0,0 +1,16 @@
+package Arrayprogram;
+
+import java.util.Arrays;
+
+public class DeleteFirst {
+	public static void main(String[] args) {
+		int []a= {2,3,9,5,6,1};
+		int []b= new int[a.length-1];
+		for(int i =0; i<b.length;i++) {
+			b[i]=a[i+1];
+		}
+		a=b;
+		System.out.println(Arrays.toString(a));
+	}
+
+}
