9135eda88e808fdb5d97fdf9af58e520c59a3270
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-09-29 23:56:44 +0530

    newly added

---
+++ Arrayprogram/Maxnumber.java
@@ -0,0 +1,15 @@
+package Arrayprogram;
+
+public class Maxnumber {
+	public static void main(String[] args) {
+		int a []= {2,3,9,5,6,10,4};
+		int max =Integer.MIN_VALUE;
+		for(int temp:a) {
+			if(temp>max) {
+				max=temp;
+			}
+		}
+		System.out.println(max);
+	}
+
+}
