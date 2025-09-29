7fbc9bf9939ec72c35b2347c84f7ea0ce8177376
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-09-29 23:47:19 +0530

    newly added

--- Arrayprogram/InsertFirst.java
+++ Arrayprogram/InsertFirst.java
@@ -1,18 +1,20 @@
+// Write a java program to insert an element at the first index of an array .
+
 package Arrayprogram;
 
 import java.util.Arrays;
 
 public class InsertFirst {
-	public static void main(String[] args) {
-		int[] a = { 2, 3, 9, 6, 5, 1 };
-		int b[] = new int[a.length + 1];
-		b[0] = 20;
-		for (int i = 1; i < b.length; i++) {
-			b[i] = a[i - 1];
-		}
-		a = b;
-		System.out.println(Arrays.toString(a));
+    public static void main(String[] args) {
+        int[] a = { 2, 3, 9, 6, 5, 1 };
+        int b[] = new int[a.length + 1];
+        b[0] = 20;
+        for (int i = 1; i < b.length; i++) {
+            b[i] = a[i - 1];
+        }
+        a = b;
+        System.out.println(Arrays.toString(a));
 
-	}
+    }
 
 }
