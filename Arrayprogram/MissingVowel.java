fcfc4ef74e66bb6a59d04ff26982d9dd2ecbb7f9
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-10-04 21:10:48 +0530

    newly added

---
+++ Arrayprogram/MissingVowel.java
@@ -0,0 +1,16 @@
+package Arrayprogram;
+
+public class MissingVowel {
+	public static void main(String[] args) {
+		String s=" java is easy";
+		String temp = "aeiou";
+		for(int i =0; i<temp.length();i++) {
+			char ch =temp.charAt(i);
+			if(s.indexOf(ch)==-1)
+			{
+				System.out.println(ch);
+			}
+		}
+	}
+
+}
