ab695fc8c09fcdd61bacc0509489c19ee6423b2a
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-10-04 21:12:43 +0530

    newly added

--- Arrayprogram/MissingVowel.java
+++ Arrayprogram/MissingVowel.java
@@ -1,16 +1,17 @@
+// Writw a java program to found the missing vowel inside a given sentense and assume that the sentense is in lower case .
+
 package Arrayprogram;
 
 public class MissingVowel {
-	public static void main(String[] args) {
-		String s=" java is easy";
-		String temp = "aeiou";
-		for(int i =0; i<temp.length();i++) {
-			char ch =temp.charAt(i);
-			if(s.indexOf(ch)==-1)
-			{
-				System.out.println(ch);
-			}
-		}
-	}
+    public static void main(String[] args) {
+        String s = " java is easy";
+        String temp = "aeiou";
+        for (int i = 0; i < temp.length(); i++) {
+            char ch = temp.charAt(i);
+            if (s.indexOf(ch) == -1) {
+                System.out.println(ch);
+            }
+        }
+    }
 
 }
